b3984d35a972b527e29c62f0c21e5fdfdff9abeb
Coder <Coder@rwadmin-H610M-HDV-M-2-R2-0>
2025-10-08 16:19:04 -0500

    feat:metodos abstractos  sobreescribidos

--- Main/src/main/java/com/mycompany/main/Dao/ProductDao.java
+++ Main/src/main/java/com/mycompany/main/Dao/ProductDao.java
@@ -3,6 +3,7 @@ package com.mycompany.main.Dao;
 import com.mycompany.main.Models.Producto;
 import com.mycompany.main.Bd.Conexion;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao implements Repositorio<Producto>{
@@ -10,10 +11,21 @@ public class ProductDao implements Repositorio<Producto>{
     @Override
     public void crear(Producto producto) {
         String sql ="INSERT INTO productos(nombre, precio, stock) VALUES (?, ?, ?)";
+        
+        
         try {
          Connection conn = Conexion.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql);
+         
+         ps.setString(1,producto.getNombre());
+         ps.setDouble(2,producto.getPrecio());
+         ps.setInt(3, producto.getStock());
+         ps.executeUpdate();
+         
+            System.out.println("producto agregado");
             
-        } catch (Exception e) {
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         
     }
@@ -25,7 +37,30 @@ public class ProductDao implements Repositorio<Producto>{
 
     @Override
     public List<Producto> buscarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "SELECT * FROM productos";
+        List<Producto> productos = new ArrayList<>();
+
+        try {
+
+            Connection conn = Conexion.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery(sql);
+
+            while (rs.next()) {
+                productos.add(new Producto(
+                        rs.getInt("id"),
+                        rs.getString("nombre"),
+                        rs.getDouble("precio"),
+                        rs.getInt("stock")
+                ));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productos;
+
     }
 
     @Override
