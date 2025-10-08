edd2273640e67538f183ed1c30870728a18723c5
Coder <Coder@rwadmin-H610M-HDV-M-2-R2-0>
2025-10-08 16:20:21 -0500

    feat:se textea un crear y un listar

--- Main/src/main/java/com/mycompany/main/Main.java
+++ Main/src/main/java/com/mycompany/main/Main.java
@@ -1,18 +1,20 @@
 package com.mycompany.main;
 
+import com.mycompany.main.Dao.ProductDao;
+import com.mycompany.main.Models.Producto;
 //import java.sql.Connection;
 //import com.mycompany.main.Bd.Conexion;
 //import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-        
-        
-        
-        
-        
-        
-        
+
+        ProductDao dao = new ProductDao();
+        Producto p = new Producto(1, "olla", 2500, 2);
+        dao.crear(p);
+
+        dao.buscarTodos().forEach(System.out::print);
 
         /*
         try {
@@ -26,6 +28,6 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        */
+         */
     }
 }
