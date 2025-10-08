dae7ac10da6edbdb5d2dd51e9e0b3d956f1b936e
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-10-08 10:32:33 -0500

    feat:formateo de los attributos y constructores

--- Main/src/main/java/com/mycompany/main/Models/Producto.java
+++ Main/src/main/java/com/mycompany/main/Models/Producto.java
@@ -4,17 +4,27 @@ package com.mycompany.main.Models;
 
 public class Producto {
     
+    private int id;
     private String nombre;
     private double  precio;
     private int stock;
 
-    public Producto(String nombre, double precio, int stock) {
+    public Producto() {
+    }
+
+    public Producto(int id, String nombre, double precio, int stock) {
+        this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
     }
 
-    public Producto() {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNombre() {
@@ -43,7 +53,9 @@ public class Producto {
 
     @Override
     public String toString() {
-        return "Producto" + "nombre=" + nombre + ", precio=" + precio + ", stock=" + stock;
+        return "Producto" + "id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", stock=" + stock ;
     }
+
+   
 
 }
