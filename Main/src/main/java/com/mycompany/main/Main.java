c7a1df733bbfd45c04f8cbefc3c07561146d2076
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-10-08 11:25:36 -0500

    feat:Texteo de la conexion

--- Main/src/main/java/com/mycompany/main/Main.java
+++ Main/src/main/java/com/mycompany/main/Main.java
@@ -1,8 +1,31 @@
 package com.mycompany.main;
 
+//import java.sql.Connection;
+//import com.mycompany.main.Bd.Conexion;
+//import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        
+        
+        
+        
+        
+        
+
+        /*
+        try {
+            Connection conn = Conexion.getConnection();
+            if (conn != null) {
+                System.out.println("conectada");
+
+            } else {
+                System.out.println("no  conectada");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        */
     }
 }
