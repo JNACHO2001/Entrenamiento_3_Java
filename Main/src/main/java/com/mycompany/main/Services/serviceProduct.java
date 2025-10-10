1722b0339368ec87fbd8997dccfb526639539bd4
Coder <Coder@usuario-Saturno-Series>
2025-10-09 19:50:12 -0500

    feat:Implementacion de los metodos y sobreescritura

--- Main/src/main/java/com/mycompany/main/Services/serviceProduct.java
+++ Main/src/main/java/com/mycompany/main/Services/serviceProduct.java
@@ -1,5 +1,25 @@
 package com.mycompany.main.Services;
 
-public class serviceProduct {
+import com.mycompany.main.Exepciones.DatoInvalidoException;
+import com.mycompany.main.Models.Producto;
+import com.mycompany.main.repository.jdbc.JdbcProductoRepository;
+
+public class serviceProduct implements ProductoServicioI {
+
+    private final JdbcProductoRepository repo;
+
+    public serviceProduct(JdbcProductoRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public Producto crearProducto(Producto p) {
+        if (p == null || p.getNombre() == null || p.getNombre().isBlank()) {
+            throw new DatoInvalidoException("Nombre requerido");
+        }
+
+        return repo.crear(p);
+
+    }
 
 }
