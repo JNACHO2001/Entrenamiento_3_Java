5147ff316dcbd6b5a7103700951a179d7ec20994
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-10-11 09:15:51 -0500

    feat:se termono la impelematacion del servicio crearProducto con la validaciones de negocio

--- Main/src/main/java/com/mycompany/main/Services/serviceProduct.java
+++ Main/src/main/java/com/mycompany/main/Services/serviceProduct.java
@@ -3,6 +3,7 @@ package com.mycompany.main.Services;
 import com.mycompany.main.Exepciones.DatoInvalidoException;
 import com.mycompany.main.Models.Producto;
 import com.mycompany.main.repository.jdbc.JdbcProductoRepository;
+import java.util.List;
 
 public class serviceProduct implements ProductoServicioI {
 
@@ -17,14 +18,36 @@ public class serviceProduct implements ProductoServicioI {
         if (p == null || p.getNombre() == null || p.getNombre().isEmpty()) {
             throw new DatoInvalidoException("Nombre requerido");
         }
-        if (p.getPrecio() < 0) {
-            throw  new DatoInvalidoException("No se permiten numeros menores o igual que 0");
-            
+        if (p.getPrecio() <= 0) {
+            throw new DatoInvalidoException("El precio debe ser mayor que 0");
+
+        }
+        if (p.getStock() < 0) {
+            throw new DatoInvalidoException("El stock no puede ser negativo");
+
         }
         return repo.crear(p);
 
-        
+    }
 
+    @Override
+    public Producto buscarProductoPorId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Producto> listarProductos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Producto actualizarProducto(Producto p) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminarProducto(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
 }
