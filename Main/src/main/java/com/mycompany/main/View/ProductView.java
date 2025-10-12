721156c867bb90429eff9ad3d5b73b7e74154666
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-10-11 19:27:22 -0500

    feat:Creacion de menu

--- Main/src/main/java/com/mycompany/main/View/ProductView.java
+++ Main/src/main/java/com/mycompany/main/View/ProductView.java
@@ -3,4 +3,27 @@ package com.mycompany.main.View;
 
 public class ProductView {
     
+    
+    
+    
+    
+    
+    
+    public String menu() {
+
+        String menu = """
+                      -------M---E---N----U-------
+                      1.Agregar
+                      2.Listar
+                      3.Actualizar
+                      4.Eliminar
+                      5.Buscar por ID
+                      6.Salir
+                      
+                      
+                      """;
+        return menu;
+
+    }
+
 }
