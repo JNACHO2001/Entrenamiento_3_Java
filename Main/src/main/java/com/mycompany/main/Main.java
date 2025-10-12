cfac411d9baf560fb9e2cedd81c0e3fa2638ef00
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-10-12 11:17:37 -0500

    feat:Se genero el cargue de la vista

--- Main/src/main/java/com/mycompany/main/Main.java
+++ Main/src/main/java/com/mycompany/main/Main.java
@@ -1,25 +1,18 @@
 package com.mycompany.main;
 
-import com.mycompany.main.repository.jdbc.JdbcProductoRepository;
-import com.mycompany.main.Models.Producto;
+//import com.mycompany.main.repository.jdbc.JdbcProductoRepository;
+//import com.mycompany.main.Models.Producto;
 //import java.sql.Connection;
 //import com.mycompany.main.Bd.Conexion;
 //import java.sql.SQLException;
+import com.mycompany.main.View.ProductView;
 
 public class Main {
 
     public static void main(String[] args) {
-        
-        
-        
-        
 
-        JdbcProductoRepository repo = new JdbcProductoRepository();
-        Producto p = new Producto(2,"", 2500, 12);
-
-        repo.crear(p);
-
-
+        ProductView vista = new ProductView();
+        vista.mostrarMenu();
 
     }
 }
